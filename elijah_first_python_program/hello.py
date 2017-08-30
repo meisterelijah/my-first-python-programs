c2111b934aa6d62f6d556b078c2ace67ddc8b7d3
meisterelijah <31486523+meisterelijah@users.noreply.github.com>
2017-08-30 14:15:05 -0400

    Add files via upload

---
+++ elijah_first_python_program/hello.py
@@ -0,0 +1,22 @@
+# This program says hello and greets a person by name.
+#
+# your name
+# August 24, 2017
+
+
+print("hello.")
+print("what is your name?")
+name = input()
+print("it is good to meet you, " + name + ".")
+name = input()
+print ("Where were you born, Elijah?")
+name = input ()
+print ("Thats cool I would like to vist greenville")
+name = input()
+print(" Do you like to play any sports?")
+name = input(y)
+name = input(n)
+y = print("thats cool")
+n = print(" I hate that sport")
+print("thats cool tell chase I said hello")
+
---
+++ elijah_first_python_program/madlib/madlib.py
@@ -0,0 +1,26 @@
+# madlib- something fun
+#
+#Elijah LeBlanc
+#August 28, 2017
+
+
+print ("give me a name of a person")
+noun1 = input()
+print (" give me another person/animal")
+noun2 = input()
+print(" give me a verb")
+verb1 = input()
+print("give me another verb")
+verb2 = input()
+print("give me the last verb")
+verb3 = input()
+print("give me an adjective")
+adjective1 = input()
+print("give me  a place")
+place = input()
+
+print (verb1 + " through the school" + noun1 + "burst into the classroom. the" +
+        adjective1 +  noun2 + " layed on the floor dead. WHY!!!!!!!" + noun1 +
+        "yelled." + verb2 + "his head into the closest desk. He picked up" + noun2 +
+        "and threw it in the garbage can in the" + place + "." + noun1 + "out of the room.")
+
